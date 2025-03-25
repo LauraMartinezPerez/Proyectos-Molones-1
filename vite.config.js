4e76d28e056252d98c20e177e7be56a0fbca3cfc
Laura <laura.laukiz@gmail.com>
2025-03-25 12:35:46 +0100

    git pages start

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {open: true,}
-})
+  server: {open: true,},
+  base: "/project-promo-48-module-3-team-3/",
+});
